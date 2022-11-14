3f781396cdb5616f9a3d5decb9661f45e6612051
robbie <robbie_mcdonough@hotmail.com>
2022-11-14 14:16:30 +0000

    nc_games.2

--- db/data/development-data/index.js
+++ db/data/development-data/index.js
@@ -1,4 +1,17 @@
-exports.categoryData = require('./categories.js');
-exports.commentData = require('./comments.js');
-exports.reviewData = require('./reviews.js');
-exports.userData = require('./users.js');
+exports.categoryData = require("./categories.js");
+exports.commentData = require("./comments.js");
+exports.reviewData = require("./reviews.js");
+exports.userData = require("./users.js");
+
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
+
+const pathToEnvFile = `${__dirname}/.env.${ENV}`;
+
+console.log(pathToEnvFile);
+
+require("dotenv").config({
+  path: pathToEnvFile,
+});
+
+module.exports = new Pool();
