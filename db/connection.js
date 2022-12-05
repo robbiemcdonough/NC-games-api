cee40deda86aa8f26fdb2c6d3f6821ca6f8cb17f
robbie <robbie_mcdonough@hotmail.com>
2022-12-05 10:59:10 +0000

    hosting api

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,19 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+      }
+    : {};
+
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+  throw new Error("PGDATABASE not set");
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
